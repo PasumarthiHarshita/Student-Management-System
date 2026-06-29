a8a815ba6d3fb2adbe70030dc6815c9afda2c5f4
Pasumarthi Harshita <pasumarthiharshita61@gmail.com>
2026-06-29 23:00:57 +0530

    Change database password to placeholder
    Updated the database password placeholder for security.

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL="jdbc:mysql://localhost:3306/student_db";
     private static final String USER="root";
-    private static final String PASSWORD="ranjiththashu@123";
+    private static final String PASSWORD="YOUR_MYSQL_PASSWORD";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL,USER,PASSWORD);
